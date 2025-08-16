6477342fa073ae0e63fd0dc48cbbec6460c14bbc
Dacrisa <dacrisa01@gmail.com>
2025-08-16 10:23:27 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,9 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "DSerna"
     val password = "Palfredo193."
+    val fromAccountId = "16674"
+    val toAccountId = "19896"
+    val amount = "10"
 
 }
+
